222200803e472805bf10680cbca7f680bc69ac2c
Tony Dang <tony@tonydang.blog>
2024-05-06 16:21:59 -0700

    add index markup

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -17,34 +17,11 @@ export async function loader({ request }: LoaderFunctionArgs) {
 }
 
 export default function Index() {
+  const currentUser = useCurrentUser();
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <h1 className="text-2xl font-bold">
+      Hi {currentUser?.email}, you are logged in!
+    </h1>
   );
 }
