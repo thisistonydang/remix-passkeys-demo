0442d25cc97baa072f6dbb6feba5d1b83681dfc6
Tony Dang <tony@tonydang.blog>
2024-05-06 16:13:44 -0700

    add markup

--- app/routes/register.tsx
+++ app/routes/register.tsx
@@ -17,3 +17,33 @@ export async function loader({ request }: LoaderFunctionArgs) {
   return {};
 }
 
+export default function Register() {
+  const actionData = useActionData<typeof action>();
+
+  return (
+    <>
+      <Form
+        method="POST"
+        className="border border-neutral rounded-lg p-5 flex flex-col gap-3 mx-auto max-w-xs"
+      >
+        <label className="form-control w-full max-w-xs">
+          <span className="label-text mb-2">Enter your email</span>
+
+          <input
+            className="input input-bordered"
+            id="email"
+            type="email"
+            name="email"
+            required
+          />
+        </label>
+
+        <button className="btn btn-accent">Register a Passkey</button>
+      </Form>
+
+      {actionData && actionData.message && (
+        <p className="text-sm text-red-500 p-2">{actionData.message}</p>
+      )}
+    </>
+  );
+}
