394972a892e86f6d8854ccdc2c42e36bf8601538
Tony Dang <tony@tonydang.blog>
2024-05-06 16:36:59 -0700

    add markup

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -9,3 +9,34 @@ import {
 
 import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
 
+export default function Login() {
+  const actionData = useActionData<typeof action>();
+
+  return (
+    <>
+      <h1 className="font-bold text-4xl mb-3">Login</h1>
+
+      <Form
+        method="POST"
+        className="border border-neutral rounded-lg p-5 flex flex-col gap-3 mx-auto max-w-xs"
+      >
+        <label className="form-control w-full max-w-xs">
+          <span className="label-text mb-2">Enter your email</span>
+
+          <input
+            className="input input-bordered"
+            id="email"
+            type="email"
+            name="email"
+          />
+        </label>
+
+        <button className="btn btn-accent">Sign in with Passkey</button>
+      </Form>
+
+      {actionData && actionData.message && (
+        <p className="text-sm text-red-500 p-2">{actionData.message}</p>
+      )}
+    </>
+  );
+}
