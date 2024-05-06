cd2376dd92886318ecd8fc41ca8073ffc529e1ca
Tony Dang <tony@tonydang.blog>
2024-05-06 16:20:15 -0700

    update meta

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,10 +6,7 @@ import { getUserSession } from "~/modules/session/session.server";
 import type { LoaderFunctionArgs, MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+  return [{ title: "Remix Passkeys Demo" }];
 };
 
 export default function Index() {
